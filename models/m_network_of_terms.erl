51f64d1712e8697707e95b86d0f336e628a2a88c
Rob van den Bogaard <robvandenbogaard@gmail.com>
2024-06-19 09:42:03 +0200

    Expose describe function on network of terms terms (#10)
    * Don't show Network of Terms tab without module usage rights
    
    * Expose describe term functionality to Erlang code too (besides in tpl)

--- models/m_network_of_terms.erl
+++ models/m_network_of_terms.erl
@@ -4,7 +4,8 @@
 -export([
     m_find_value/3,
     m_to_list/2,
-    m_value/2
+    m_value/2,
+    describe/2
 ]).
 
 -include_lib("zotonic.hrl").
@@ -18,21 +19,29 @@ m_find_value(sources, #m{}, Context) ->
 m_find_value(Uri, #m{}, Context) when not is_binary(Uri) ->
     m_find_value(z_convert:to_binary(Uri), #m{}, Context);
 m_find_value(Uri, #m{}, Context) when is_binary(Uri) ->
-    z_depcache:memo(
-        fun() ->
-            [Result] = network_of_terms_client:lookup([Uri], Context),
-            lookup_result(Result)
-        end,
-        {term, Uri},
-        ?WEEK,
-        Context
-    ).
+    describe(Uri, Context).
 
 m_to_list(_Arg0, _Arg1) ->
     erlang:error(not_implemented).
 
 m_value(_Arg0, _Arg1) ->
     erlang:error(not_implemented).
+
+describe(undefined, _Context) ->
+    undefined;
+describe(Id, Context) when is_integer(Id) ->
+    Uri = m_rsc:p_no_acl(Id, uri, Context),
+    describe(Uri, Context);
+describe(Uri, Context) ->
+    z_depcache:memo(
+      fun() ->
+              [Result] = network_of_terms_client:lookup([Uri], Context),
+              lookup_result(Result)
+      end,
+      {term, Uri},
+      ?WEEK,
+      Context
+     ).
 
 lookup_result(#{<<"result">> := #{<<"__typename">> := <<"NotFoundError">>}}) ->
     undefined;
